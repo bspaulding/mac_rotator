65a4cdaada12fecf4efac7f2be8888c24b86323d
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-03-10 17:55:22 -0400

    Initial commit.

---
+++ lib/mac-rotator.rb
@@ -0,0 +1,7 @@
+require "mac-rotator/version"
+
+module Mac
+  module Rotator
+    # Your code goes here...
+  end
+end
---
+++ lib/mac-rotator/version.rb
@@ -0,0 +1,5 @@
+module Mac
+  module Rotator
+    VERSION = "0.0.1"
+  end
+end
